5bbde932acf19b6f9b33e89e749e2feb3e175064
Pun - Prim Yaemklud <prim.ya@worldrewardsolutions.com>
2022-03-12 12:36:20 +0700

    update login

--- pages/authen/login.vue
+++ pages/authen/login.vue
@@ -1,66 +1,106 @@
 <template>
-<div>
+  <div>
     <div class="h-screen flex">
-        <div class="flex w-1/2 bg-gradient-to-tr from-blue-800 to-purple-700 i justify-around items-center">
-            <div>
-                <h1 class="text-white font-bold text-4xl font-sans">Follow Me</h1>
-                <p class="text-white mt-1">ระบบการฝึกเคลื่อนไหวร่างกายแบบออนไลน์ by NECTEC</p>
-                <button type="submit" class="block w-28 bg-white text-indigo-800 mt-4 py-2 rounded-2xl font-bold mb-2">Alpha v.01a</button>
-            </div>
+      <div
+        class="
+          flex
+          w-1/2
+          bg-gradient-to-tr
+          from-blue-800
+          to-purple-700
+          i
+          justify-around
+          items-center
+        "
+      >
+        <div>
+          <h1 class="text-white font-bold text-4xl font-sans">Follow Me</h1>
+          <p class="text-white mt-1">
+            ระบบการฝึกเคลื่อนไหวร่างกายแบบออนไลน์ by NECTEC
+          </p>
+          <button
+            type="submit"
+            class="
+              block
+              w-28
+              bg-white
+              text-indigo-800
+              mt-4
+              py-2
+              rounded-2xl
+              font-bold
+              mb-2
+            "
+          >
+            Alpha v.01a
+          </button>
         </div>
-        <div class="flex w-1/2 justify-center items-center bg-white">
-            <form class="bg-white">
-                <h1 class="text-gray-800 font-bold text-2xl mb-1">Sign In</h1>
-                <p class="text-sm font-normal text-gray-600 mb-7">Welcome Back</p>
+      </div>
+      <div class="flex w-1/2 justify-center items-center bg-white">
+        <form class="bg-white">
+          <h1 class="text-gray-800 font-bold text-2xl mb-1">Sign In</h1>
+          <p class="text-sm font-normal text-gray-600 mb-7">Welcome Back</p>
 
-                <form @submit.prevent="login()">
-                    <v-text-field required v-model="form.username" outlined name="name" label="Username" id="id"></v-text-field>
-                    <v-text-field required v-model="form.password" outlined name="name" label="Password" id="id"></v-text-field>
-                    <v-btn block type="submit" color="success">เข้าสู่ระบบ</v-btn>
-                </form>
-                <span class="text-sm ml-2 hover:text-blue-500 cursor-pointer">Forgot Password ?</span>
-            </form>
-        </div>
+          <form @submit.prevent="login()">
+            <v-text-field
+              required
+              v-model="form.username"
+              outlined
+              name="name"
+              label="Username"
+              id="id"
+            ></v-text-field>
+            <v-text-field
+              required
+              v-model="form.password"
+              outlined
+              name="name"
+              label="Password"
+              id="id"
+            ></v-text-field>
+            <v-btn block type="submit" color="success">เข้าสู่ระบบ</v-btn>
+          </form>
+          <v-btn class="mt-7 text-sm" block color="primary" >สมัครสมาชิก</v-btn>
+          <span class="text-sm ml-2 hover:text-blue-500 cursor-pointer"
+            >Forgot Password ?</span
+          >
+        </form>
+      </div>
     </div>
-</div>
+  </div>
 </template>
 
 <script>
-import {
-    Core
-} from '@/vuexes/core'
-import {
-    Auth
-} from '@/vuexes/auth'
+import { Core } from "@/vuexes/core";
+import { Auth } from "@/vuexes/auth";
 export default {
-    layout: 'root',
-    data: () => ({
-        form: {}
-    }),
-    async created() {
-        await this.checkUser();
+  layout: "root",
+  data: () => ({
+    form: {},
+  }),
+  async created() {
+    await this.checkUser();
+  },
+  methods: {
+    async login() {
+      await Auth.login(this.form);
+      let user = await Auth.getUser();
+      console.log(user);
+      if (user.pk) {
+        //await location.reload();
+        await this.$router.replace("/");
+        await location.reload();
+      }
     },
-    methods: {
-        async login() {
-            await Auth.login(this.form);
-            let user = await Auth.getUser();
-            console.log(user);
-            if (user.pk) {
-                //await location.reload();
-                await this.$router.replace('/')
-                await location.reload();
-            }
-        },
-        async checkUser() {
-            let user = await Auth.getUser();
-            if (user.pk) {
-                await this.$router.replace('/')
-            }
-        }
-    }
-}
+    async checkUser() {
+      let user = await Auth.getUser();
+      if (user.pk) {
+        await this.$router.replace("/");
+      }
+    },
+  },
+};
 </script>
 
 <style>
-
 </style>
